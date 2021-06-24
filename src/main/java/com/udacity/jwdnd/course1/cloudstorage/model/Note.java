67ecf606ca400164a33b967da71abf4fc1a9daf4
Zhurylo Mikhaylo <dir2000@yjr.net>
2021-06-24 13:40:54 +0300

    initial commit

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Note.java
@@ -0,0 +1,8 @@
+package com.udacity.jwdnd.course1.cloudstorage.model;
+
+public class Note {
+    private Integer noteId;
+    private String noteTitle;
+    private String noteDescription;
+    private Integer userId;
+}
